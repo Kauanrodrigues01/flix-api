009e10d32bc3f25a70e1a901c5c152414263a179
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-15 14:31:57 -0300

    feat: add validation to prevent genre deletion with related movies

--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+from django.http import JsonResponse
 
 class GenreListCreateView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
@@ -17,3 +18,9 @@ class GenreListCreateView(generics.ListCreateAPIView):
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    
+    def destroy(self, request, *args, **kwargs):
+        genre = self.get_object()
+        if genre.movies.exists():
+            return JsonResponse({'error': 'Cannot delete genre with related movies.'}, status=status.HTTP_502_BAD_GATEWAY)
+        return super().destroy(request, *args, **kwargs)
