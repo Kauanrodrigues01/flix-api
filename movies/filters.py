40ef7dba575a3d50c58b144fc86e04ac2d52ec7e
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-15 16:52:59 -0300

    update movies filters

--- movies/filters.py
+++ movies/filters.py
@@ -5,10 +5,10 @@ from actors.models import Actor
 
 class MovieFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='title', lookup_expr='icontains')
-    genre = filters.ModelChoiceFilter(queryset=Genre.objects.all())
+    genre = filters.ModelChoiceFilter(queryset=Genre.objects.all().only('id'))
     release_date = filters.DateFromToRangeFilter(field_name='release_date')
     release_year = filters.NumberFilter(field_name='release_date', lookup_expr='year')
-    actors = filters.ModelMultipleChoiceFilter(queryset=Actor.objects.all())
+    actors = filters.ModelMultipleChoiceFilter(queryset=Actor.objects.all().only('id'))
     
     class Meta:
         model = Movie
