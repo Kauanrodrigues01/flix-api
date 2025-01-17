14f1b667b90e242031fbaf29ecd49823c3f5ba91
Kauan Rodrigues <kauanrl09@gmail.com>
2025-01-17 13:31:20 -0300

    reset migrations

--- movies/models.py
+++ movies/models.py
@@ -10,6 +10,7 @@ class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
     release_date = models.DateField(null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name='movies')
+    resume = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.title
