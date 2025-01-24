ea71e14eaae195db4331e54bdd1519025265b92a
Kauan Rodrigues <kauanrl09@gmail.com>
2025-01-23 21:46:04 -0300

    feat: added general movie statistics endpoint

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,6 @@ app_name = 'movies'
 
 urlpatterns = [
     path('movies/', views.MovieListCreateView.as_view(), name='movies-list-create'),
-    path('movies/<uuid:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='movies-retrieve-update-destroy')
+    path('movies/<uuid:pk>/', views.MovieRetrieveUpdateDestroyView.as_view(), name='movies-retrieve-update-destroy'),
+    path('movies/stats/', views.MovieStatsView.as_view(), name="movie-stats-view")
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,10 @@
+from django.db.models import Count, Avg
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics, views, response, status
 
 from app.permissions import GlobalDefaultModelPermission
 from utils.pagination import create_pagination_class
+from reviews.models import Review
 from .filters import MovieFilter
 from .models import Movie
 from .serializers import MovieSerializer
@@ -28,8 +29,29 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     def destroy(self, request, *args, **kwargs):
         movie = self.get_object()
         if movie.reviews.exists():
-            return Response(
+            return response.Response(
                 {'error': 'Cannot delete movie with related reviews.'},
                 status=status.HTTP_502_BAD_GATEWAY
             )
         return super().destroy(request, *args, **kwargs)
+
+
+class MovieStatsView(views.APIView):
+    queryset = Movie.objects.select_related('genre').prefetch_related('actors').all()
+    permission_classes = [GlobalDefaultModelPermission]
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(average_stars=Avg('stars'))['average_stars']
+
+        return response.Response(
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': list(movies_by_genre),
+                'total_reviews': total_reviews,
+                'average_stars': round(average_stars, 2) if average_stars else 0,
+            },
+            status=200
+        )
